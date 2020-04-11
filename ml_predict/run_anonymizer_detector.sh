7ad968e0fbeacdc15cf6f7b2d5f003ce248f2518
Daniele Gadler <daniele.gadler@yahoo.it>
2020-04-11 12:07:37 +0200

    Renamed image

--- ml_predict/run_anonymizer_detector.sh
+++ ml_predict/run_anonymizer_detector.sh
@@ -3,5 +3,5 @@ conda activate virus
 #Run anonymizer on the input image
 PYTHONPATH=$PYTHONPATH:. python ./anonymizer/bin/anonymize.py --input ./1-input-images  --image-output ./2-anonymized-images --weights ./weights
 #Run people detector on the input image
-python people_detector.py --image_input=./2-anonymized-images/farmacia_queue_GB.jpg --image_output=./3-people-detected-images/bounding_boxes_image.jpg
+python people_detector.py --image_input=./2-anonymized-images/image_people.jpg --image_output=./3-people-detected-images/bounding_boxes_image.jpg
 
